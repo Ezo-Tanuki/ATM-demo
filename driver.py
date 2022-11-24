6d9aa8ae7c427b2226666824ee4324b46b694d17
Ezo-Tanuki <michailbryanlie@gmail.com>
2022-11-24 12:40:31 +0700

    Remove unnecessary imports

--- driver.py
+++ driver.py
@@ -1,11 +1,8 @@
 from atmModule import ATM
-from customer import Customer
-from admin import Administrator
 
 def main():
-    mik = ATM("JJ", "123456")
+    mik = ATM("ATM", "123456")
     mik.start()
-
 
 if __name__ == "__main__":
     main()
